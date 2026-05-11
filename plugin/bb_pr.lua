235fdf9e154a1e9ca0be1b7e226a1a302f44331e
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-05-11 14:24:20 +0300

    abb

--- plugin/bb_pr.lua
+++ plugin/bb_pr.lua
@@ -1,8 +1,8 @@
 if vim.g.loaded_bb_pr == 1 then
-  return
+	return
 end
 vim.g.loaded_bb_pr = 1
 
 require("bb_pr").setup({
-  provider_cmd = { "bb", "-reviewers", "-json" },
+	provider_cmd = { "bb", "-reviewers", "-json" },
 })
